92ca2f23879dda8faa0164e75c31b8e829a9fd42
Bar Horing Amir <bar.apple@gmail.com>
2020-05-20 20:15:43 +0300

    done ? Reducer Action Keys & duspatch

--- src/NewGrudge.js
+++ src/NewGrudge.js
@@ -4,7 +4,9 @@ const NewGrudge = ({ onSubmit }) => {
   const [person, setPerson] = useState('');
   const [reason, setReason] = useState('');
 
-  const handleChange = event => {
+  console.log('Rendering new Grudge');
+
+  const handleChange = (event) => {
     event.preventDefault();
     onSubmit({ person, reason });
   };
@@ -16,14 +18,14 @@ const NewGrudge = ({ onSubmit }) => {
         placeholder="Person"
         type="text"
         value={person}
-        onChange={event => setPerson(event.target.value)}
+        onChange={(event) => setPerson(event.target.value)}
       />
       <input
         className="NewGrudge-input"
         placeholder="Reason"
         type="text"
         value={reason}
-        onChange={event => setReason(event.target.value)}
+        onChange={(event) => setReason(event.target.value)}
       />
       <input className="NewGrudge-submit button" type="submit" />
     </form>
